59c62520be2c8896864292b49bb728c84c1b5bf8
Vparonline <74346832+Vparonline@users.noreply.github.com>
2021-03-24 00:05:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,207 @@
+const { Discord, MessageEmbed, Client } = require('discord.js');
+const client = new Client({fetchAllMembers: true});
+const db = require("quick.db");
+const fs = require("fs");
+const ayar = require("./settings.json");
+
+client.on('ready', async () => {
+    client.user.setPresence({ activity: { name: ayar.GenelAyarlar.BotDurum }, status: "online" });
+    client.channels.cache.get(ayar.GenelAyarlar.BotSesKanali).join();
+});
+
+const commands = new Map();
+global.commands = commands;
+const aliases = new Map();
+global.aliases = aliases;
+global.client = client;
+fs.readdir("./Commands", (err, files) => {
+    if(err) return console.error(err);
+    files = files.filter(file => file.endsWith(".js"));
+    console.log(`${files.length} komut yüklenecek.`);
+    files.forEach(file => {
+        let prop = require(`./Commands/${file}`);
+        if(!prop.configuration) return;
+        console.log(`${prop.configuration.name} komutu yükleniyor!`);
+        if(typeof prop.onLoad === "function") prop.onLoad(client);
+        commands.set(prop.configuration.name, prop);
+        if(prop.configuration.aliases) prop.configuration.aliases.forEach(aliase => aliases.set(aliase, prop));
+    });
+});
+
+fs.readdir("./Events", (err, files) => {
+    if(err) return console.error(err);
+    files.filter(file => file.endsWith(".js")).forEach(file => {
+        let prop = require(`./Events/${file}`);
+        if(!prop.configuration) return;
+        client.on(prop.configuration.name, prop);
+    });
+});
+fs.readdir("./Controllers", (err, files) => {
+  if(err) return console.error(err);
+  files.filter(file => file.endsWith(".js")).forEach(file => {
+      let prop = require(`./Controllers/${file}`);
+      if(!prop.configuration) return;
+      client.on(prop.configuration.name, prop);
+  });
+});
+
+
+  client.on("message", (message) => {
+        if (message.author.bot ||!message.content.startsWith(ayar.GenelAyarlar.prefix) || !message.channel || message.channel.type == "dm") return;
+        let args = message.content
+          .substring(ayar.GenelAyarlar.prefix.length)
+          .split(" ");
+        let command = args[0];
+        let bot = message.client;
+        args = args.splice(1);
+        let calistirici;
+        if (commands.has(command)) {
+          calistirici = commands.get(command);
+          calistirici.execute(bot, message, args);
+        } else if (aliases.has(command)) {
+          calistirici = aliases.get(command);
+          calistirici.execute(bot, message, args);
+        }
+  });
+
+Date.prototype.toTurkishFormatDate = function (format) {
+    let date = this,
+      day = date.getDate(),
+      weekDay = date.getDay(),
+      month = date.getMonth(),
+      year = date.getFullYear(),
+      hours = date.getHours(),
+      minutes = date.getMinutes(),
+      seconds = date.getSeconds();
+  
+    let monthNames = new Array("Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık");
+    let dayNames = new Array("Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi");
+  
+    if (!format) {
+      format = "dd MM yyyy | hh:ii:ss";
+    };
+    format = format.replace("mm", month.toString().padStart(2, "0"));
+    format = format.replace("MM", monthNames[month]);
+    
+    if (format.indexOf("yyyy") > -1) {
+      format = format.replace("yyyy", year.toString());
+    } else if (format.indexOf("yy") > -1) {
+      format = format.replace("yy", year.toString().substr(2, 2));
+    };
+    
+    format = format.replace("dd", day.toString().padStart(2, "0"));
+    format = format.replace("DD", dayNames[weekDay]);
+  
+    if (format.indexOf("HH") > -1) format = format.replace("HH", hours.toString().replace(/^(\d)$/, '0$1'));
+    if (format.indexOf("hh") > -1) {
+      if (hours > 12) hours -= 12;
+      if (hours === 0) hours = 12;
+      format = format.replace("hh", hours.toString().replace(/^(\d)$/, '0$1'));
+    };
+    if (format.indexOf("ii") > -1) format = format.replace("ii", minutes.toString().replace(/^(\d)$/, '0$1'));
+    if (format.indexOf("ss") > -1) format = format.replace("ss", seconds.toString().replace(/^(\d)$/, '0$1'));
+    return format;
+  };
+  let kufurler = ["allahoc","allahoç","allahamk","allahaq","0r0spuc0cu","4n4n1 sk3r1m","p1c","@n@nı skrm","evladi","orsb","orsbcogu","amnskm","anaskm","oc","abaza","abazan","ag","a\u011fz\u0131na s\u0131\u00e7ay\u0131m","fuck","shit","ahmak","seks","sex","allahs\u0131z","amar\u0131m","ambiti","am biti","amc\u0131\u011f\u0131","amc\u0131\u011f\u0131n","amc\u0131\u011f\u0131n\u0131","amc\u0131\u011f\u0131n\u0131z\u0131","amc\u0131k","amc\u0131k ho\u015faf\u0131","amc\u0131klama","amc\u0131kland\u0131","amcik","amck","amckl","amcklama","amcklaryla","amckta","amcktan","amcuk","am\u0131k","am\u0131na","amına","am\u0131nako","am\u0131na koy","am\u0131na koyar\u0131m","am\u0131na koyay\u0131m","am\u0131nakoyim","am\u0131na koyyim","am\u0131na s","am\u0131na sikem","am\u0131na sokam","am\u0131n feryad\u0131","am\u0131n\u0131","am\u0131n\u0131 s","am\u0131n oglu","am\u0131no\u011flu","am\u0131n o\u011flu","am\u0131s\u0131na","am\u0131s\u0131n\u0131","amina","amina g","amina k","aminako","aminakoyarim","amina koyarim","amina koyay\u0131m","amina koyayim","aminakoyim","aminda","amindan","amindayken","amini","aminiyarraaniskiim","aminoglu","amin oglu","amiyum","amk","amkafa","amk \u00e7ocu\u011fu","amlarnzn","aml\u0131","amm","ammak","ammna","amn","amna","amnda","amndaki","amngtn","amnn","amona","amq","ams\u0131z","amsiz","amsz","amteri","amugaa","amu\u011fa","amuna","ana","anaaann","anal","analarn","anam","anamla","anan","anana","anandan","anan\u0131","anan\u0131","anan\u0131n","anan\u0131n am","anan\u0131n am\u0131","anan\u0131n d\u00f6l\u00fc","anan\u0131nki","anan\u0131sikerim","anan\u0131 sikerim","anan\u0131sikeyim","anan\u0131 sikeyim","anan\u0131z\u0131n","anan\u0131z\u0131n am","anani","ananin","ananisikerim","anani sikerim","ananisikeyim","anani sikeyim","anann","ananz","anas","anas\u0131n\u0131","anas\u0131n\u0131n am","anas\u0131 orospu","anasi","anasinin","anay","anayin","angut","anneni","annenin","annesiz","anuna","aq","a.q","a.q.","aq.","ass","atkafas\u0131","atm\u0131k","att\u0131rd\u0131\u011f\u0131m","attrrm","auzlu","avrat","ayklarmalrmsikerim","azd\u0131m","azd\u0131r","azd\u0131r\u0131c\u0131","babaannesi ka\u015far","baban\u0131","baban\u0131n","babani","babas\u0131 pezevenk","baca\u011f\u0131na s\u0131\u00e7ay\u0131m","bac\u0131na","bac\u0131n\u0131","bac\u0131n\u0131n","bacini","bacn","bacndan","bacy","bastard","b\u0131z\u0131r","bitch","biting","boner","bosalmak","bo\u015falmak","cenabet","cibiliyetsiz","cibilliyetini","cibilliyetsiz","cif","cikar","cim","\u00e7\u00fck","dalaks\u0131z","dallama","daltassak","dalyarak","dalyarrak","dangalak","dassagi","diktim","dildo","dingil","dingilini","dinsiz","dkerim","domal","domalan","domald\u0131","domald\u0131n","domal\u0131k","domal\u0131yor","domalmak","domalm\u0131\u015f","domals\u0131n","domalt","domaltarak","domalt\u0131p","domalt\u0131r","domalt\u0131r\u0131m","domaltip","domaltmak","d\u00f6l\u00fc","d\u00f6nek","d\u00fcd\u00fck","eben","ebeni","ebenin","ebeninki","ebleh","ecdad\u0131n\u0131","ecdadini","embesil","emi","fahise","fahi\u015fe","feri\u015ftah","ferre","fuck","fucker","fuckin","fucking","gavad","gavat","giberim","giberler","gibis","gibi\u015f","gibmek","gibtiler","goddamn","godo\u015f","godumun","gotelek","gotlalesi","gotlu","gotten","gotundeki","gotunden","gotune","gotunu","gotveren","goyiim","goyum","goyuyim","goyyim","g\u00f6t","g\u00f6t deli\u011fi","g\u00f6telek","g\u00f6t herif","g\u00f6tlalesi","g\u00f6tlek","g\u00f6to\u011flan\u0131","g\u00f6t o\u011flan\u0131","g\u00f6to\u015f","g\u00f6tten","g\u00f6t\u00fc","g\u00f6t\u00fcn","g\u00f6t\u00fcne","g\u00f6t\u00fcnekoyim","g\u00f6t\u00fcne koyim","g\u00f6t\u00fcn\u00fc","g\u00f6tveren","g\u00f6t veren","g\u00f6t verir","gtelek","gtn","gtnde","gtnden","gtne","gtten","gtveren","hasiktir","hassikome","hassiktir","has siktir","hassittir","haysiyetsiz","hayvan herif","ho\u015faf\u0131","h\u00f6d\u00fck","hsktr","huur","\u0131bnel\u0131k","ibina","ibine","ibinenin","ibne","ibnedir","ibneleri","ibnelik","ibnelri","ibneni","ibnenin","ibnerator","ibnesi","idiot","idiyot","imansz","ipne","iserim","i\u015ferim","ito\u011flu it","kafam girsin","kafas\u0131z","kafasiz","kahpe","kahpenin","kahpenin feryad\u0131","kaka","kaltak","kanc\u0131k","kancik","kappe","karhane","ka\u015far","kavat","kavatn","kaypak","kayyum","kerane","kerhane","kerhanelerde","kevase","keva\u015fe","kevvase","koca g\u00f6t","kodu\u011fmun","kodu\u011fmunun","kodumun","kodumunun","koduumun","koyarm","koyay\u0131m","koyiim","koyiiym","koyim","koyum","koyyim","krar","kukudaym","laciye boyad\u0131m","libo\u015f","madafaka","malafat","malak","mcik","meme","memelerini","mezveleli","minaamc\u0131k","mincikliyim","mna","monakkoluyum","motherfucker","mudik","oc","ocuu","ocuun","O\u00c7","o\u00e7","o. \u00e7ocu\u011fu","o\u011flan","o\u011flanc\u0131","o\u011flu it","orosbucocuu","orospu","orospucocugu","orospu cocugu","orospu \u00e7oc","orospu\u00e7ocu\u011fu","orospu \u00e7ocu\u011fu","orospu \u00e7ocu\u011fudur","orospu \u00e7ocuklar\u0131","orospudur","orospular","orospunun","orospunun evlad\u0131","orospuydu","orospuyuz","orostoban","orostopol","orrospu","oruspu","oruspu\u00e7ocu\u011fu","oruspu \u00e7ocu\u011fu","osbir","ossurduum","ossurmak","ossuruk","osur","osurduu","osuruk","osururum","otuzbir","\u00f6k\u00fcz","\u00f6\u015fex","patlak zar","penis","pezevek","pezeven","pezeveng","pezevengi","pezevengin evlad\u0131","pezevenk","pezo","pic","pici","picler","pi\u00e7","pi\u00e7in o\u011flu","pi\u00e7 kurusu","pi\u00e7ler","pipi","pipi\u015f","pisliktir","porno","pussy","pu\u015ft","pu\u015fttur","rahminde","revizyonist","s1kerim","s1kerm","s1krm","sakso","saksofon","saxo","sekis","serefsiz","sevgi koyar\u0131m","sevi\u015felim","sexs","s\u0131\u00e7ar\u0131m","s\u0131\u00e7t\u0131\u011f\u0131m","s\u0131ecem","sicarsin","sie","sik","sikdi","sikdi\u011fim","sike","sikecem","sikem","siken","sikenin","siker","sikerim","sikerler","sikersin","sikertir","sikertmek","sikesen","sikesicenin","sikey","sikeydim","sikeyim","sikeym","siki","sikicem","sikici","sikien","sikienler","sikiiim","sikiiimmm","sikiim","sikiir","sikiirken","sikik","sikil","sikildiini","sikilesice","sikilmi","sikilmie","sikilmis","sikilmi\u015f","sikilsin","sikim","sikimde","sikimden","sikime","sikimi","sikimiin","sikimin","sikimle","sikimsonik","sikimtrak","sikin","sikinde","sikinden","sikine","sikini","sikip","sikis","sikisek","sikisen","sikish","sikismis","siki\u015f","siki\u015fen","siki\u015fme","sikitiin","sikiyim","sikiym","sikiyorum","sikkim","sikko","sikleri","sikleriii","sikli","sikm","sikmek","sikmem","sikmiler","sikmisligim","siksem","sikseydin","sikseyidin","siksin","siksinbaya","siksinler","siksiz","siksok","siksz","sikt","sikti","siktigimin","siktigiminin","sikti\u011fim","sikti\u011fimin","sikti\u011fiminin","siktii","siktiim","siktiimin","siktiiminin","siktiler","siktim","siktim","siktimin","siktiminin","siktir","siktir et","siktirgit","siktir git","siktirir","siktiririm","siktiriyor","siktir lan","siktirolgit","siktir ol git","sittimin","sittir","skcem","skecem","skem","sker","skerim","skerm","skeyim","skiim","skik","skim","skime","skmek","sksin","sksn","sksz","sktiimin","sktrr","skyim","slaleni","sokam","sokar\u0131m","sokarim","sokarm","sokarmkoduumun","sokay\u0131m","sokaym","sokiim","soktu\u011fumunun","sokuk","sokum","soku\u015f","sokuyum","soxum","sulaleni","s\u00fclaleni","s\u00fclalenizi","s\u00fcrt\u00fck","\u015ferefsiz","\u015f\u0131ll\u0131k","taaklarn","taaklarna","tarrakimin","tasak","tassak","ta\u015fak","ta\u015f\u015fak","tipini s.k","tipinizi s.keyim","tiyniyat","toplarm","topsun","toto\u015f","vajina","vajinan\u0131","veled","veledizina","veled i zina","verdiimin","weled","weledizina","whore","xikeyim","yaaraaa","yalama","yalar\u0131m","yalarun","yaraaam","yarak","yaraks\u0131z","yaraktr","yaram","yaraminbasi","yaramn","yararmorospunun","yarra","yarraaaa","yarraak","yarraam","yarraam\u0131","yarragi","yarragimi","yarragina","yarragindan","yarragm","yarra\u011f","yarra\u011f\u0131m","yarra\u011f\u0131m\u0131","yarraimin","yarrak","yarram","yarramin","yarraminba\u015f\u0131","yarramn","yarran","yarrana","yarrrak","yavak","yav\u015f","yav\u015fak","yav\u015fakt\u0131r","yavu\u015fak","y\u0131l\u0131\u015f\u0131k","yilisik","yogurtlayam","yo\u011furtlayam","yrrak","z\u0131kk\u0131m\u0131m","zibidi","zigsin","zikeyim","zikiiim","zikiim","zikik","zikim","ziksiiin","ziksiin","zulliyetini","zviyetini"];
+  client.chatKoruma = async mesajIcerik => {
+  if (!mesajIcerik) return;
+    let inv = /(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/([a-z0-9-.]+)?/i;  
+    if (inv.test(mesajIcerik)) return true;
+
+    let link = /(http[s]?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/gi;  
+    if (link.test(mesajIcerik)) return true;
+
+    if ((kufurler).some(word => new RegExp("(\\b)+(" + word + ")+(\\b)", "gui").test(mesajIcerik))) return true;
+  return false;
+};
+  
+
+const sayiEmojiler = {
+  0: "0",
+  1: "1",
+  2: "2",
+  3: "3",
+  4: "4",
+  5: "5",
+  6: "6",
+  7: "7",
+  8: "8",
+  9: "9"
+};
+
+client.emojiSayi = function(sayi) {
+  var yeniMetin = "";
+  var arr = Array.from(sayi);
+  for (var x = 0; x < arr.length; x++) {
+    yeniMetin += (sayiEmojiler[arr[x]] === "" ? arr[x] : sayiEmojiler[arr[x]]);
+  }
+  return yeniMetin;
+};
+
+client.tarihHesapla = (date) => {
+  const startedAt = Date.parse(date);
+  var msecs = Math.abs(new Date() - startedAt);
+
+  const years = Math.floor(msecs / (1000 * 60 * 60 * 24 * 365));
+  msecs -= years * 1000 * 60 * 60 * 24 * 365;
+  const months = Math.floor(msecs / (1000 * 60 * 60 * 24 * 30));
+  msecs -= months * 1000 * 60 * 60 * 24 * 30;
+  const weeks = Math.floor(msecs / (1000 * 60 * 60 * 24 * 7));
+  msecs -= weeks * 1000 * 60 * 60 * 24 * 7;
+  const days = Math.floor(msecs / (1000 * 60 * 60 * 24));
+  msecs -= days * 1000 * 60 * 60 * 24;
+  const hours = Math.floor(msecs / (1000 * 60 * 60));
+  msecs -= hours * 1000 * 60 * 60;
+  const mins = Math.floor((msecs / (1000 * 60)));
+  msecs -= mins * 1000 * 60;
+  const secs = Math.floor(msecs / 1000);
+  msecs -= secs * 1000;
+
+  var string = "";
+  if (years > 0) string += `${years} yıl ${months} ay`
+  else if (months > 0) string += `${months} ay ${weeks > 0 ? weeks+" hafta" : ""}`
+  else if (weeks > 0) string += `${weeks} hafta ${days > 0 ? days+" gün" : ""}`
+  else if (days > 0) string += `${days} gün ${hours > 0 ? hours+" saat" : ""}`
+  else if (hours > 0) string += `${hours} saat ${mins > 0 ? mins+" dakika" : ""}`
+  else if (mins > 0) string += `${mins} dakika ${secs > 0 ? secs+" saniye" : ""}`
+  else if (secs > 0) string += `${secs} saniye`
+  else string += `saniyeler`;
+
+  string = string.trim();
+  return `\`${string} önce\``;
+};
+
+const iltifatlar = [
+  'Gözlerindeki saklı cenneti benden başkası fark etsin istemiyorum.',
+  'Dünyanın en tatlı varlığı sen misin ?',
+  'Cennetten düşen bir kar tanesi gibisin',
+  'Dünya fani ölüm ani bir kere sevsem ne olur yani',
+  'Merhaba dünyanın en mükemmel insanı',
+  'Seni çok güzel bir zaafsın',
+  'Sabaha kadar sana iltifat edebilirim',
+];
+client.on("message", async message => {
+  if(message.channel.id !== ayar.GenelAyarlar.GenelChatID) return;
+  let winneiiltifat = db.get('chatiltifat');
+  await db.add("chatiltifat", 1);
+  if(winneiiltifat>= 250) { 
+    db.delete("chatiltifat");
+    const random = Math.floor(Math.random() * ((iltifatlar).length - 1) + 1);
+    message.reply(`${(iltifatlar)[random]}`);
+  };
+});
+
+
+
+client.on("messageDelete", async(message) => {
+  if (message.channel.type === "dm" || !message.guild || message.author.bot) return;
+let snipe = {
+mesaj: message.content,
+mesajyazan: message.author.id,
+ytarihi: message.createdTimestamp,
+starihi: Date.now(), 
+kanal: message.channel.id
+}
+await db.set(`snipe.${message.guild.id}`, snipe)
+}); 
+client.login(ayar.GenelAyarlar.token).then(console.log("Bot başarılı bir şekilde giriş yaptı.")).catch(e => console.error(e));
